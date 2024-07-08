44e93dd51840d910b4caa37df0d652e6107a707a
Andrew Cornforth <andrew.cornforth@shift.online>
2024-07-08 19:53:39 +0100

    force domain (testing)

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -81,7 +81,7 @@ export const authOptions: NextAuthOptions = {
         sameSite: "lax",
         path: "/",
         // When working on localhost, the cookie domain must be omitted entirely (https://stackoverflow.com/a/1188145)
-        domain: VERCEL_DEPLOYMENT ? ".invest.jacob-corlett.co.uk" : undefined,
+        domain: ".invest.jacob-corlett.co.uk",
         secure: VERCEL_DEPLOYMENT,
       },
     },
